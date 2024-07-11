e8832534dce264a60955ba958035df76837a6ce6
Ridhoo <smkridhopandu@gmail.com>
2024-07-11 09:34:41 +0700

    Fixing Error Navbar Active Menu

--- resources/views/components/Modal/register.blade.php
+++ resources/views/components/Modal/register.blade.php
@@ -25,6 +25,10 @@
 
                         <input name="name" class="appearance-none border border-gray-300 rounded-lg w-full py-3 px-4 placeholder-serv-text text-xs"
                             id="username" type="text" placeholder="Your name" required>
+
+                            @if ($errors->has('name'))
+                                <p class="text-red-500 mb-3 text-sm">{{ $errors->first('name') }}</p>
+                            @endif
                     </div>
 
                     <div class="mb-4">
@@ -34,6 +38,10 @@
 
                         <input name="email" class="appearance-none border border-gray-300 rounded-lg w-full py-3 px-4 placeholder-serv-text text-xs"
                             id="email" type="email" placeholder="name@domain.com" required>
+
+                            @if ($errors->has('email'))
+                                <p class="text-red-500 mb-3 text-sm">{{ $errors->first('email') }}</p>
+                            @endif
                     </div>
 
                     <div>
@@ -43,6 +51,10 @@
 
                         <input name="password" class="appearance-none border border-gray-300 rounded-lg w-full py-3 px-4 placeholder-serv-text text-xs mb-3"
                             id="password" type="password" placeholder="At least 8 characters" required>
+
+                            @if ($errors->has('password'))
+                                <p class="text-red-500 mb-3 text-sm">{{ $errors->first('password') }}</p>
+                            @endif
                     </div>
                     <div>
                         <label class="block text-grey-darker text-sm mb-2" for="password_confirmation">
@@ -51,6 +63,10 @@
 
                         <input name="password_confirmation" class="appearance-none border border-gray-300 rounded-lg w-full py-3 px-4 placeholder-serv-text text-xs mb-3"
                             id="password_confirmation" type="password" placeholder="At least 8 characters" required>
+
+                            @if ($errors->has('password_confirmation'))
+                                <p class="text-red-500 mb-3 text-sm">{{ $errors->first('password_confirmation ') }}</p>
+                            @endif
                     </div>
 
                     <div class="flex items-center justify-between">
