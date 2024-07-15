b9a33feb8cb8cf7ee9be3e2307e4f8baacb50eb4
Ridhoo <smkridhopandu@gmail.com>
2024-07-15 14:03:12 +0700

    Development Profile - Part 2

--- app/Http/Controllers/Dashboard/ProfileController.php
+++ app/Http/Controllers/Dashboard/ProfileController.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-use File; 
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Auth;
 
 use App\Models\User;
@@ -33,7 +33,12 @@ class ProfileController extends Controller
      */
     public function index()
     {
-        return view('pages.dashboard.profile');
+        $user = User::where('id', Auth::user()->id)->first();
+        $experience_user = ExperienceUser::where('detail_user_id', $user->detail_user->id)
+                                ->orderBy('id', 'asc')
+                                ->get();
+
+        return view('pages.dashboard.profile', compact('user', 'experience_user'));
     }
 
     /**
@@ -74,9 +79,40 @@ class ProfileController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(UpdateProfileRequest $request_profile, UpdateDetailUserRequest $request_detail_user)
     {
-        //
+        $data_profile = $request_profile->all();
+        $data_detail_user = $request_detail_user->all();
+
+        // get photo user
+        $get_photo = DetailUser::where('users_id', Auth::user()->id)->first() ;
+
+
+        // delete old file from storage
+        if(isset($data_detail_user['photo'])){
+                $data = 'storage/'.$get_photo['photo'];
+                if(File::exists($data)){
+                        File::delete($data);
+                }else{
+                    File::delete('storage/app/public/').$get_photo['photo'];
+                }
+
+        }
+
+        // store file to storage
+        if(isset($data_detail_user['photo'])){
+                $data_detail_user['photo'] = $request_detail_user->file('photo')->store(
+                    'assets/photo', 'public'
+                );
+        }
+
+        // save to user
+        $user = User::find(Auth::user()->id);
+        $user->update($data_profile);
+
+        // save to detail user   
+        $detail_user = DetailUser::find($user->detail_user->id);
+        $detail_user->update($data_detail_user);
     }
 
     /**
