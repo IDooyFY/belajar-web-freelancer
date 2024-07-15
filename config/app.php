9f0bc82a2359a4a2a9d1a6cd337ad5fa96c8eae2
Ridhoo <smkridhopandu@gmail.com>
2024-07-15 10:44:28 +0700

    Install & Config SweetAlert

--- config/app.php
+++ config/app.php
@@ -159,6 +159,7 @@ return [
         /*
          * Package Service Providers...
          */
+        RealRashid\SweetAlert\SweetAlertServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -184,7 +185,10 @@ return [
     */
 
     'aliases' => Facade::defaultAliases()->merge([
-        // 'Example' => App\Facades\Example::class,
+
+        'Alert' => RealRashid\SweetAlert\Facades\Alert::class,
+        
     ])->toArray(),
+    
 
 ];
